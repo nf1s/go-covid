4cac729679a4ba30a8ea72522853972a162dfd7e
Nana Osei Yeboah <yeboahnanaosei@gmail.com>
2020-04-16 19:23:04 +0000

    Return errors instead of log.Fataln

--- covid/covid.go
+++ covid/covid.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -98,71 +97,75 @@ func getTotalUrl(field string) string {
 	return _url.String()
 }
 
-func GetData() []Case {
+func GetData() ([]Case, error) {
 	resp, err := http.Get(getAllUrl())
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	var res Response
 	json.Unmarshal(body, &res)
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
 
-	return res.Values
+	return res.Values, nil
 }
 
-func GetCountryById(id int) Case {
+func GetCountryById(id int) (Case, error) {
 	resp, err := http.Get(getCountryUrl(id))
 	if err != nil {
-		log.Fatalln(err)
+		return Case{}, err
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalln(err)
+		return Case{}, err
 	}
 	defer resp.Body.Close()
 
 	var res Response
-	json.Unmarshal(body, &res)
+	err = json.Unmarshal(body, &res)
 	if err != nil {
-		log.Fatalln(err)
+		return Case{}, err
 	}
 
-	return res.Values[0]
+	return res.Values[0], nil
 }
 
-func ListCountries() []Country {
+func ListCountries() ([]Country, error) {
 	resp, err := http.Get(getAllUrl())
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	var res CountryResponse
-	json.Unmarshal(body, &res)
+	err = json.Unmarshal(body, &res)
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
 
-	return res.Values
+	return res.Values, nil
 }
 
 func getCountryId(name string) (int, error) {
-	countries := ListCountries()
+	countries, err := ListCountries()
+	if err != nil {
+		return -1, err
+	}
+
 	for _, country := range countries {
 		if strings.ToLower(country.Attrs.Name) == strings.ToLower(name) {
 			return country.Attrs.Id, nil
@@ -171,67 +174,79 @@ func getCountryId(name string) (int, error) {
 	return -1, errors.New("Wrong country name")
 }
 
-func GetCountryByName(name string) Case {
+func GetCountryByName(name string) (Case, error) {
 	id, err := getCountryId(name)
 	if err != nil {
-		log.Fatalln(err)
+		return Case{}, err
 	}
 
 	resp, err := http.Get(getCountryUrl(id))
 	if err != nil {
-		log.Fatalln(err)
+		return Case{}, err
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalln(err)
+		return Case{}, err
 	}
 	defer resp.Body.Close()
 
 	var res Response
-	json.Unmarshal(body, &res)
+	err = json.Unmarshal(body, &res)
 	if err != nil {
-		log.Fatalln(err)
+		return Case{}, err
 	}
 
-	return res.Values[0]
+	return res.Values[0], nil
 }
 
-func getTotal(field string) Stats {
+func getTotal(field string) (Stats, error) {
 	resp, err := http.Get(getTotalUrl(field))
 	if err != nil {
-		log.Fatalln(err)
+		return Stats{}, err
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalln(err)
+		return Stats{}, err
 	}
 	defer resp.Body.Close()
 
 	var res StatsResponse
-	json.Unmarshal(body, &res)
+	err = json.Unmarshal(body, &res)
 	if err != nil {
-		log.Fatalln(err)
+		return Stats{}, err
 	}
 
-	return res.Values[0]
+	return res.Values[0], nil
 }
 
-func GetTotalActive() int {
-	data := getTotal("Active")
-	return data.Attrs.Value
+func GetTotalActive() (int, error) {
+	data, err := getTotal("Active")
+	if err != nil {
+		return -1, err
+	}
+	return data.Attrs.Value, nil
 }
 
-func GetTotalConfirmed() int {
-	data := getTotal("Confirmed")
-	return data.Attrs.Value
+func GetTotalConfirmed() (int, error) {
+	data, err := getTotal("Confirmed")
+	if err != nil {
+		return -1, err
+	}
+	return data.Attrs.Value, err
 }
-func GetTotalRecovered() int {
-	data := getTotal("Recovered")
-	return data.Attrs.Value
+func GetTotalRecovered() (int, error) {
+	data, err := getTotal("Recovered")
+	if err != nil {
+		return -1, err
+	}
+	return data.Attrs.Value, nil
 }
-func GetTotalDeaths() int {
-	data := getTotal("Deaths")
-	return data.Attrs.Value
+func GetTotalDeaths() (int, error) {
+	data, err := getTotal("Deaths")
+	if err != nil {
+		return -1, err
+	}
+	return data.Attrs.Value, nil
 }
